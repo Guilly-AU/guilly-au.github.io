a08d3330377abe79ec8e294d17f5936998b2ed02
Guilly-AU <125476287+Guilly-AU@users.noreply.github.com>
2023-08-11 16:38:44 +0800

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,16 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 
 function AppHeader() {
   return (
-    <Navbar
-      expand="lg"
-      fixed="top"
-      bg="myPrimaryLight"
-      className="jsutify-content-between"
-    >
+    <Navbar expand="md" bg="myPrimaryLight">
       <Container>
-        <Navbar.Brand className="text-primary fs-3 fw-bold " href="#home">
+        <Navbar.Brand className="text-primary fs-3 fw-bold" href="#home">
           GD
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="flex-grow-0 text-center"
+        >
           <Nav className="me-auto fs-5">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About me</Nav.Link>
